761d8e9348741b6cce0bfae3ed000fe4c3d97123
Navneet Lal Gupta <navneetlalg@gmail.com>
2021-08-26 16:38:42 +0530

    fix: removed script.js && 0 to false

--- script.js
+++
@@ -1,8 +0,0 @@
-const trains = require('./data/trains.json')
-const fs = require('fs')
-
-const x = {
-    ...trains,
-    features: trains.features.map(f => ({ type: f.type, properties: f.properties, geometry: f.geometry }))
-}
-fs.writeFileSync('trains.json', JSON.stringify(x))
