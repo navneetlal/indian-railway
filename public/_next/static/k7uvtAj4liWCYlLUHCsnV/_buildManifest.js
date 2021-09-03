cd90bb4853f5bf1cd7d1a8edf2fd4590d0a8f1d1
Navneet Lal Gupta <navneetlalg@gmail.com>
2021-09-03 20:10:45 +0530

    update: updated api documentation

--- public/_next/static/k7uvtAj4liWCYlLUHCsnV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-d65bbff8d9392a16d396.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
