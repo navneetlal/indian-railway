ceda5b7744e2ec40904027fea1ec4e7e153dfe92
Navneet Lal Gupta <navneetlalg@gmail.com>
2021-09-04 17:06:22 +0530

    fix: enabled cors

--- services/allowCors.ts
+++ services/allowCors.ts
@@ -6,7 +6,7 @@ const allowCors = (fn: any) => async (req: any, res: any) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
     if (req.method === 'OPTIONS') {
       res.status(200).end()
